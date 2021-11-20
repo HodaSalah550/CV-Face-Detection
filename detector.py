ce8684a3a41f8dc5ed69a1fbc3c649f95bf9857b
Hoda Salah <43321584+HodaSalah550@users.noreply.github.com>
2021-11-20 23:23:17 +0200

    Update detector.py

--- detector.py
+++ detector.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 """
 Created on Sat Nov 20 20:16:12 2021
 
@@ -7,10 +6,10 @@ Created on Sat Nov 20 20:16:12 2021
 import cv2
 
 #create a CascadeClassifier Object
-face_cascade =  cv2.CascadeClassifier("C:\\Users\\Hoda\\FaceDetection\\Features\haarcascade_frontalface_default.xml")
+face_cascade =  cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
 
 #Reading the image as it is 
-img = cv2.imread("C:\\Users\\Hoda\\FaceDetection\\lena.jpg")
+img = cv2.imread("lena.jpg")
 
 #Reading the image in grayscale
 gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
@@ -24,13 +23,13 @@ print (faces)
 for x,y,w,h in faces:
     img = cv2.rectangle(img, (x,y), (x+w,y+h), (0,255,0), 1)
 """
-(x,y) makan ma habd2 arsam el rect.
-(x+w,y+h) end of rect. 
-y3ni w and h atwal el rect mn el ganben
+(x,y) start edge of the rectangle
+(x+w,y+h) end of rectangle 
+w and h are the length of the drawn rectangle
 
-(0,255,0) ==> BGR value of the rect. outline
+(0,255,0) ==> BGR value of the rectangle outline
 
-1 ==> width of the rect.
+1 ==> width of the rectangle
 """
 
 #resized = cv2.resize(img, (int(img.shape[1]*2),int(img.shape[0]*2)))
